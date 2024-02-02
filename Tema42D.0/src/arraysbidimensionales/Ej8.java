90ae42649705adc0ec4cd86a2ed895b26c3e2111
Marco55H <145675712+Marco55H@users.noreply.github.com>
2024-02-02 14:38:23 +0100

    q

--- Tema42D.0/src/arraysbidimensionales/Ej8.java
+++ Tema42D.0/src/arraysbidimensionales/Ej8.java
@@ -1,5 +1,30 @@
 package arraysbidimensionales;
 
 public class Ej8 {
-
+	public static void main(String[] args) {
+		//Creamos la tabla 
+		int tabla[][] = new int[4][4];
+		
+		for(int i =0;i<tabla.length;i++) {
+			for(int j=0;j<tabla[i].length;j++) {
+				tabla[i][j]=1;
+			}
+		}
+		
+	}
+	public static boolean igual(int tabla[][]) {
+		boolean igual=true;
+		
+		
+		for(int i=0;i<tabla.length;i++) {
+			for(int j=0;j<tabla[i].length;j++) {
+				if (tabla[i][j]==tabla[j][i]) {
+					igual=false;
+				}
+			}
+		}
+		
+		return igual;	
+	}
+	
 }
